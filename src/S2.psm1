2a646a86f4c0ed0d974e15db943110b47dbe30c4
Charles <crossan007@gmail.com>
2017-09-01 11:39:00 -0400

    quiet logout

--- src/S2.psm1
+++ src/S2.psm1
@@ -59,7 +59,7 @@ function Connect-S2Service {
 
 function Disconnect-S2Service {
     $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"Logout`" num=`"1`" dateformat=`"tzoffset`"></COMMAND></NETBOX-API>"
-    Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml
+    Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml | Out-Null
 }
 
 
