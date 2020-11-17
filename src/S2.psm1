a5cc72c6c68d749dd9c6812f9b44073110e04c29
Bradley Herbst <bordwalk2000@gmail.com>
2020-11-17 14:20:46 -0600

    Fixed Small Formating Issues

--- src/S2.psm1
+++ src/S2.psm1
@@ -150,12 +150,12 @@ Function Get-S2Person {
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS
 }
 
-function Get-S2AccessLevels {
+Function Get-S2AccessLevels {
     $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"GetAccessLevels`" num=`"1`"></COMMAND></NETBOX-API>"
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS.ACCESSLEVELS.ACCESSLEVEL
 }
 
-function Get-S2CardFormats {
+Function Get-S2CardFormats {
     $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"GetCardFormats`" num=`"1`"></COMMAND></NETBOX-API>"
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS.CARDFORMATS.CARDFORMAT
 }
