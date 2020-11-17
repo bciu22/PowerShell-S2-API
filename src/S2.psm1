05c52f592c0f595924a481c0d84e4ecc2ff655e0
Bradley Herbst <bordwalk2000@gmail.com>
2020-11-17 14:28:28 -0600

    Added S2Credential Functions
    Added functions that allow for either adding
    or removing a credntial (card number) to a
    user.

--- src/S2.psm1
+++ src/S2.psm1
@@ -200,3 +200,75 @@ Function Get-S2PersonPhoto {
         $Photo.content
     }
 }
+
+Function Add-S2Credential {
+    param(
+        [parameter(Mandatory=$true)][String] $PersonID,
+        [parameter(Mandatory=$true)][String] $CardFormat,
+        [parameter(Mandatory=$true)][Int] $CardNumber
+    )
+
+    [xml]$xml = New-Object System.Xml.XmlDocument
+    $wrapper = $xml.AppendChild($xml.CreateElement("NETBOX-API"))
+    $wrapper.SetAttribute("sessionid",$NETBOXSessionID)
+    $command = $wrapper.AppendChild($xml.CreateElement("COMMAND"))
+    $Command.SetAttribute("name","AddCredential")
+    $Command.SetAttribute("num","1")
+    $Parameters=$Command.AppendChild($xml.CreateElement("PARAMS"))
+    $param = $xml.CreateElement("PERSONID")
+    $param.innerText = $PersonID
+    $Parameters.AppendChild($param) | Out-Null
+    $param = $xml.CreateElement("CARDFORMAT")
+    $param.innerText = $CardFormat
+    $Parameters.AppendChild($param) | Out-Null
+    $param = $xml.CreateElement("ENCODEDNUM")
+    $param.innerText = $CardNumber
+    $Parameters.AppendChild($param) | Out-Null
+   
+    $Response = Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml
+    $ReturnXML = $([xml]$Response.content)
+
+    switch ($ReturnXML.NETBOX.RESPONSE.CODE) {
+        "SUCCESS" { Return $true  }
+        Default { 
+            Write-Output $ReturnXML.NETBOX.RESPONSE.CODE
+            Return $false 
+        }
+    }
+}
+
+Function Remove-S2Credential {
+    param(
+        [parameter(Mandatory=$true)][String] $PersonID,
+        [parameter(Mandatory=$true)][String] $CardFormat,
+        [parameter(Mandatory=$true)][Int] $CardNumber
+    )
+
+    [xml]$xml = New-Object System.Xml.XmlDocument
+    $wrapper = $xml.AppendChild($xml.CreateElement("NETBOX-API"))
+    $wrapper.SetAttribute("sessionid",$NETBOXSessionID)
+    $command = $wrapper.AppendChild($xml.CreateElement("COMMAND"))
+    $Command.SetAttribute("name","RemoveCredential")
+    $Command.SetAttribute("num","1")
+    $Parameters=$Command.AppendChild($xml.CreateElement("PARAMS"))
+    $param = $xml.CreateElement("PERSONID")
+    $param.innerText = $PersonID
+    $Parameters.AppendChild($param) | Out-Null
+    $param = $xml.CreateElement("CARDFORMAT")
+    $param.innerText = $CardFormat
+    $Parameters.AppendChild($param) | Out-Null
+    $param = $xml.CreateElement("ENCODEDNUM")
+    $param.innerText = $CardNumber
+    $Parameters.AppendChild($param) | Out-Null
+   
+    $Response = Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml
+    $ReturnXML = $([xml]$Response.content)
+
+    switch ($ReturnXML.NETBOX.RESPONSE.CODE) {
+        "SUCCESS" { Return $true }
+        Default { 
+            Write-Output $ReturnXML.NETBOX.RESPONSE.CODE
+            Return $false 
+        }
+    }
+}
