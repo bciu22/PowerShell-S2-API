9dfd3a35d6a2a9cce2a758caaf045c15a80223e7
Bradley Herbst <bradley.herbst@ametek.com>
2020-11-17 13:32:33 -0600

    Formatting Changes & Connect Help
    Changed the formating of some of the functions, changed whitespace formatting
    as well ad added help to the Connect-S2Service funtion.

--- src/S2.psm1
+++ src/S2.psm1
@@ -1,29 +1,36 @@
 <#
-    .SYNOPSIS
+ .Synopsis
+  Module for interfacing with the S2 NETBOX API.
 
+ .Description
+  Module for enterying and querying information from the S2 NETBOX API.
+
+ .Parameter Username
+  Valid username of an S2 Account
+
+ .Parameter Password
+  Corresponding password to specified username
+
+ .Parameter S2Hostname
+  The network location to the S2 Netbox API server.
+
+ .Parameter S2Protocol
+  Specifiy what http protocal to use.  By default it uses http.
+
+ .Link
+  Forked from bciu22/PowerShell-S2-API
+  https://github.com/bciu22/PowerShell-S2-API/blob/master/src/S2.psm1
 #>
-
-
-function Connect-S2Service {
+Function Connect-S2Service {
     <#
-
     .PARAMETER Username
-
     .PARAMETER Password
-
     #>
     param (
-        [parameter(Mandatory=$true)]
-        [String]
-        $Username,
-        [parameter(Mandatory=$true)]
-        [String]
-        $Password,
-        [parameter(Mandatory=$true)]
-        [String]
-        $S2HOSTNAME,
-        [String]
-        $S2PROTOCOL = "https://"
+        [parameter(Mandatory=$true)][String] $Username,
+        [parameter(Mandatory=$true)][String] $Password,
+        [parameter(Mandatory=$true)][String] $S2Hostname,
+        [String] $S2Protocol = "http://"
     )
     Set-Variable -Scope Global -Name "S2HOSTNAME" -Value $S2HOSTNAME
     Set-Variable -Scope Global -Name "S2PROTOCOL" -Value $S2PROTOCOL
@@ -45,7 +52,7 @@ function Connect-S2Service {
     $Response = Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $($xml.innerXML)
     $ReturnXML = $([xml]$Response.content)
 
-    if ($ReturnXML.NETBOX.RESPONSE.APIERROR)
+    if($ReturnXML.NETBOX.RESPONSE.APIERROR)
     {
         throw "Unable to connect to NETBOX-API.  Error code: $($ReturnXML.NETBOX.RESPONSE.APIERROR)"
     }
@@ -53,25 +60,23 @@ function Connect-S2Service {
     {
         Set-Variable -Scope Global -Name "NETBOXSessionID" -Value $($ReturnXML.NETBOX.sessionid)
     }
-    
-
 }
 
-function Disconnect-S2Service {
+Function Disconnect-S2Service {
     $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"Logout`" num=`"1`" dateformat=`"tzoffset`"></COMMAND></NETBOX-API>"
     Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml | Out-Null
 }
 
 
 Function Search-S2Person {
-    Param(
-        $LastName,
-        $MiddleName,
-        $FirstName,
-        $PersonID,
-        $UDF1,
-        $UDF2,
-        $UDF3
+    param(
+        [String] $LastName,
+        [String] $MiddleName,
+        [String] $FirstName,
+        [String] $PersonID,
+        [String] $UDF1,
+        [String] $UDF2,
+        [String] $UDF3
     )
 
     [xml]$xml = New-Object System.Xml.XmlDocument
@@ -83,76 +88,73 @@ Function Search-S2Person {
     $Command.SetAttribute("dateformat","tzoffset")
     $Parameters=$Command.AppendChild($xml.CreateElement("PARAMS"))
 
-    if ($LastName)
+    if($LastName)
     {
         $param = $xml.CreateElement("LASTNAME")
         $param.innerText = $LastName
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
     if($MiddleName)
     {
         $param = $xml.CreateElement("MIDDLENAME")
         $param.innerText = $MiddleName
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
-    if ($FirstName)
+    if($FirstName)
     {
         $param = $xml.CreateElement("FIRSTNAME")
         $param.innerText = $FirstName
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
-    if ($PersonID)
+    if($PersonID)
     {
         $param = $xml.CreateElement("PERSONID")
         $param.innerText = $PersonID
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
-    if ($UDF1)
+    if($UDF1)
     {
         $param = $xml.CreateElement("UDF1")
         $param.innerText = $UDF1
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
     if($UDF2)
     {
         $param = $xml.CreateElement("UDF2")
         $param.innerText = $UDF2
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
     if($UDF3)
     {
         $param = $xml.CreateElement("UDF3")
         $param.innerText = $UDF3
-        $Parameters.AppendChild($Param) | Out-Null
+        $Parameters.AppendChild($param) | Out-Null
     }
 
     $param = $xml.CreateElement("CASEINSENSITIVE")
     $param.innerText = "TRUE"
-    $Parameters.AppendChild($Param) | Out-Null
+    $Parameters.AppendChild($param) | Out-Null
 
     $param = $xml.CreateElement("WILDCARDSEARCH")
     $param.innerText = "TRUE"
-    $Parameters.AppendChild($Param) | Out-Null
+    $Parameters.AppendChild($param) | Out-Null
 
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml.innerXML).content).NETBOX.RESPONSE.DETAILS.PEOPLE.PERSON
-
 }
 
-function Get-S2Person {
-    Param(
+Function Get-S2Person {
+    param(
         $PersonID
     )
     $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"GetPerson`" num=`"1`" dateformat=`"tzoffset`"><PARAMS><PERSONID>$PersonID</PERSONID></PARAMS></COMMAND></NETBOX-API>"
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS
-
 }
 
-function Get-S2PersonPhoto {
-    Param(
+Function Get-S2PersonPhoto {
+    param(
         $PersonID,
         $OutFile,
-        [Switch]
-        $Display
+        [Switch]$Display
     )
     $Person = Get-S2Person -PersonID $PersonID
 
@@ -166,7 +168,7 @@ function Get-S2PersonPhoto {
     $S2URL = "$($S2PROTOCOL)$($S2HOSTNAME)/upload/pics/$($Person.PICTUREURL)"
     $Photo = Invoke-WebRequest -URI $S2URL -Method GET -WebSession $session
 
-    if ($Display)
+    if($Display)
     {
         $Stream =[System.IO.MemoryStream]::new($Photo.content)
         $img = [System.Drawing.Image]::FromStream($Stream)
@@ -180,7 +182,7 @@ function Get-S2PersonPhoto {
         $win.AutoSize = $true
         $win.ShowDialog()
     }
-    elseif ($OutFile)
+    elseif($OutFile)
     {
         [System.IO.File]::WriteAllBytes($OutFile,$Photo.content)
     }
