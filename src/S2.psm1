dd12c0ea54c6f571f9c920f89e5e0e191f6c0d16
Bradley Herbst <bordwalk2000@gmail.com>
2020-11-17 20:02:09 -0600

    Updated Get AccessLevel Functions
    Added a get AccessLevel by ID function as well as a parameter on
    the Get-S2AccessLevels that returns the ID instead of the Names.
    I had to run the results of the IDs though a foreach loop of the
    Get-S2AccessLevel function to associate ID with Names.
    
    Get-S2AccessLevels -WantKey |
    % {
    Write-Output "ACCESSLEVELID          : $_";
    Get-S2AccessLevel -AccessLevelKey $_
    }

--- src/S2.psm1
+++ src/S2.psm1
@@ -149,8 +149,33 @@ Function Get-S2Person {
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS
 }
 
+Function Get-S2AccessLevel {
+    param(
+        [parameter(Mandatory=$true)][String] $AccessLevelKey
+        # [parameter(Mandatory=$true)][String] $ACCESSLEVELNAME
+    )
+    $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"GetAccessLevel`" num=`"1`"><PARAMS><ACCESSLEVELKEY>$AccessLevelKey</ACCESSLEVELKEY></PARAMS></COMMAND></NETBOX-API>"
+    $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS
+}
+
 Function Get-S2AccessLevels {
-    $xml = "<NETBOX-API sessionid=`"$NETBOXSessionID`"><COMMAND name=`"GetAccessLevels`" num=`"1`"></COMMAND></NETBOX-API>"
+    param(
+        [Switch] $WantKey
+    )
+    [xml]$xml = New-Object System.Xml.XmlDocument
+    $wrapper = $xml.AppendChild($xml.CreateElement("NETBOX-API"))
+    $wrapper.SetAttribute("sessionid",$NETBOXSessionID)
+    $command = $wrapper.AppendChild($xml.CreateElement("COMMAND"))
+    $Command.SetAttribute("name","GetAccessLevels")
+    $Command.SetAttribute("num","1")
+    if($WantKey)
+    {
+        $Parameters=$Command.AppendChild($xml.CreateElement("PARAMS"))
+        $param = $xml.CreateElement("WANTKEY")
+        $param.innerText = ("TRUE")
+        $Parameters.AppendChild($param) | Out-Null
+    }
+
     $([XML]$(Invoke-WebRequest -URI "$($S2PROTOCOL)$($S2HOSTNAME)/goforms/nbapi" -Method Post -Body $xml).content).NETBOX.RESPONSE.DETAILS.ACCESSLEVELS.ACCESSLEVEL
 }
 
